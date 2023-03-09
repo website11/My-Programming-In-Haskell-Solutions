3f425c2d77dbf6270da27d324ec7f8e3ad946eda
website11 <72529146+website11@users.noreply.github.com>
2023-03-09 11:27:32 -0500

    Create ex_2.hs

---
+++ Ch 3/ex_2.hs
@@ -0,0 +1,8 @@
+
+--Write down definitions that have the following types; it does not matter what the definitions actually do as long as they are type correct.
+
+bools = [True,True]
+nums = [[1,2,3],[1,3,2]]
+add a b c = a + b + c
+copy a = (a,a)
+apply f x = f x
