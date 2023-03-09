117c74ccc0dd7fd22b938885f558453160b0f829
website11 <72529146+website11@users.noreply.github.com>
2023-03-09 11:28:02 -0500

    Create ex_3.hs

---
+++ Ch 3/ex_3.hs
@@ -0,0 +1,7 @@
+-- What are the types of the following functions?
+second :: [a] -> a
+swap :: (a,b) -> (b,a)
+pair :: a -> b -> (a,b)
+double ::a -> a
+palindrome [a] -> Bool
+twice :: (a -> a) -> a -> a
